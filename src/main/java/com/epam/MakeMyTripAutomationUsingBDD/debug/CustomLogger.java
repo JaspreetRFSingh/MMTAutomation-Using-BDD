62250a41e8a6a662d2edbeedb522a5f9fb133c79
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2019-12-09 23:23:38 +0530

    Debugging..

---
+++ src/main/java/com/epam/MakeMyTripAutomationUsingBDD/debug/CustomLogger.java
@@ -0,0 +1,7 @@
+package com.epam.MakeMyTripAutomationUsingBDD.debug;
+
+import org.apache.log4j.Logger;
+
+public class CustomLogger {
+	public static Logger logger = Logger.getLogger("MMTAutomation");
+}
